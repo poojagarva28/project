e3623b8a35c4f362716aced107dc761761e81fdb
Pooja Garva <poojagarva123@gmail.com>
2022-07-18 14:10:19 +0530

    optimized login logout functionality

--- js/checkLogin.js
+++ js/checkLogin.js
@@ -1,12 +1,11 @@
 if (localStorage.getItem("loginStatus") === "false") {
-  window.alert("Please Login First");
   window.location.href = "./index.html";
+  window.alert("Please Login First");
 }
 
 let action = document.getElementById("action");
 action.addEventListener("click", function () {
   if (localStorage.getItem("loginStatus") == "true") {
-    console.log("action cliced");
-    window.location.href = "./orders.html";
+    window.location.href = this.pathname;
   }
 });
